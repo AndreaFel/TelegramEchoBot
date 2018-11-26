8fcf25b16ec473d94711e2a0b1f3a071df2fa144
Andrea <and.felline@gmail.com>
2018-11-26 22:46:19 +0100

    prova 1

--- execute.php
+++ execute.php
@@ -14,7 +14,7 @@ $firstname = isset($message['chat']['first_name']) ? $message['chat']['first_nam
 $lastname = isset($message['chat']['last_name']) ? $message['chat']['last_name'] : "";
 $username = isset($message['chat']['username']) ? $message['chat']['username'] : "";
 $date = isset($message['date']) ? $message['date'] : "";
-$text = isset($message['text']) ? "che vuol dire ".$message['text']."?" : "";
+$text = isset($message['text']) ? "enniente" : "";
 
 $text = trim($text);
 $text = strtolower($text);
