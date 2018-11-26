f291bb4b03f41501240b14a3ddfbe9e6bfc8b751
AndreaFel <and.felline@gmail.com>
2018-11-26 19:19:36 +0100

    Update execute.php

--- execute.php
+++ execute.php
@@ -14,7 +14,7 @@ $firstname = isset($message['chat']['first_name']) ? $message['chat']['first_nam
 $lastname = isset($message['chat']['last_name']) ? $message['chat']['last_name'] : "";
 $username = isset($message['chat']['username']) ? $message['chat']['username'] : "";
 $date = isset($message['date']) ? $message['date'] : "";
-$text = isset($message['text']) ? $message['text']."aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" : "";
+$text = isset($message['text']) ? $message['text'] : "";
 
 $text = trim($text);
 $text = strtolower($text);
